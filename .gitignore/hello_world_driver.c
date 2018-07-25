b54f877dc03a15d67fd164ef4a7ac383103f5af7
TheLittleNaruto <TheLittleNaruto@users.noreply.github.com>
2018-07-25 14:48:29 +0530

    removed return type to exit module

--- .gitignore/hello_world_driver.c
+++ .gitignore/hello_world_driver.c
@@ -2,18 +2,13 @@
 #include<linux/module.h>
 
 static int hello_init(void){
-
   printk( KERN_ALERT "Hello World Loaded. \n" );
   return 0;
-
 }
 
 
-static int hello_exit(void){
-
+static void hello_exit(void){
   printk( KERN_ALERT "Goodbye, cruel world\n");
-  return 0;
-
 }
 
 module_init(hello_init);
