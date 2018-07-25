7d3f8b4194d6622ebe46748375e152cca1d0869c
TheLittleNaruto <TheLittleNaruto@users.noreply.github.com>
2018-07-25 14:47:40 +0530

    create hello_world_driver

---
+++ .gitignore/hello_world_driver.c
@@ -0,0 +1,20 @@
+#include<linux/init.h>
+#include<linux/module.h>
+
+static int hello_init(void){
+
+  printk( KERN_ALERT "Hello World Loaded. \n" );
+  return 0;
+
+}
+
+
+static int hello_exit(void){
+
+  printk( KERN_ALERT "Goodbye, cruel world\n");
+  return 0;
+
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
